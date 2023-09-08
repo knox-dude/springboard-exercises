17b10d647be2ee5faeb663e7fdbe8eb08b366a9a
Andrew Knox <andrew.knox.r@gmail.com>
2023-09-08 14:50:41 -0700

    adding random colors demo completed

--- randomColorsDemo/app.js
+++ randomColorsDemo/app.js
@@ -1,2 +1,29 @@
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256)
+  const g = Math.floor(Math.random() * 256)
+  const b = Math.floor(Math.random() * 256)
+  return `rgb(${r},${g},${b})`
+}
+
 const h1 = document.querySelector('h1');
-h1.style.color = 'red';
+h1.style.color = randomRGB();
+h1.style.fontSize='80px'
+
+setInterval(function() {
+  h1.style.color = randomRGB();
+}, 500)
+
+const letters = document.querySelectorAll('.letter')
+
+for (let letter of letters) {
+  setInterval(function() {
+    letter.style.color = randomRGB();
+  }, 500)
+}
+
+const intervalId = setInterval(function() {
+  for (let letter of letters) {
+    letter.style.color = randomRGB();
+  }
+}, 500)
+
