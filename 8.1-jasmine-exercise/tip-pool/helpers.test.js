796321b4bea254b3cd60cad167789bbe942bdb21
Andrew Knox <andrew.knox.r@gmail.com>
2023-09-27 14:01:00 -0700

    adding portion of further study

---
+++ 8.1-jasmine-exercise/tip-pool/helpers.test.js
@@ -0,0 +1,8 @@
+describe('Helpers test (with setup and tear-down)', () => {
+  beforeEach(() => {
+    billAmtInput = '30';
+    tipAmtInput = '10';
+  })
+
+  // it ('should calculate')
+})
--- 8.1-jasmine-exercise/tip-pool/servers.test.js
+++ 8.1-jasmine-exercise/tip-pool/servers.test.js
@@ -11,7 +11,19 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should create table row elements and append them to serverTbody on updateServerTable()', () => {
+    submitServerInfo();
+    updateServerTable();
+
+    let curServerTbody = document.querySelectorAll('#serverTable tbody tr td');
+    expect(curServerTbody.length).toEqual(2);
+    expect(curServerTbody[0].innerText).toEqual('Alice');
+    expect(curServerTbody[1].innerText).toEqual('$0.00')
+  })
+
   afterEach(function() {
-    // teardown logic
+    allServers = {};
+    serverId = 0;
+    serverTbody.innerHTML='';
   });
 });
