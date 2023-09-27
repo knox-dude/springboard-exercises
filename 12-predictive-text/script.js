4dafb7939b9f981684bbf8096fde566627280791
Andrew Knox <andrew.knox.r@gmail.com>
2023-09-26 20:49:36 -0700

    adding finished project

--- 12-predictive-text/script.js
+++ 12-predictive-text/script.js
@@ -3,68 +3,77 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-// returns all fruit that have the user's input as a substring
+// get entries in fruit that have str as a substring
 function search(str) {
+	if (str === '') return [];
 	let results = [];
 	results = fruit.filter(val => (val.toLowerCase().indexOf(str) != -1));
 	return results;
 }
 
-// Function that displays search bar on keyup event
+// Function that displays search results on keyup event
 function searchHandler(e) {
 	const userInput = e.target.value.toLowerCase();
-
-	// get list of fruit that match the user's search
-	const fruitRemaining = search(userInput);
-
-	// clear the outdated list from last search
-	suggestions.innerHTML = '';
-
-	// show suggestions for current search
-	showSuggestions(fruitRemaining, userInput);
+	const fruitRemaining = search(userInput); 	// filter from list fruit
+	suggestions.innerHTML = ''; 								// clear outdated list from last search
+	showSuggestions(fruitRemaining, userInput); // show suggestions for current search
 }
 
-// Populates suggestions list
+/**
+ * Populates suggestions with bolded text search results.
+ * @param {Array} results - list of entries in fruit that have inputVal as a substring.
+ * @param {string} inputVal - lowercased input from the search bar.
+ */
 function showSuggestions(results, inputVal) {
-
-	// results is all fruit that match userInput
 	for (let result of results) {
 		const newLi = document.createElement('li');
-
-		//turn userInput part of result into bold 
-		const boldedResult = boldSubstring(result, inputVal)
+		const boldedResult = boldSubstring(result, inputVal) //turn userInput part of result into bold 
 		newLi.innerHTML = boldedResult;
-
 		suggestions.appendChild(newLi);
 	}
 }
 
-// returns str with substr in bold
+/**
+ * Surrounds the string subStr within str with \<b>\</b>
+ * @param {string} str - string with substring subStr
+ * @param {string} subStr - string used to find bold idx
+ * @returns {string} returns a copy of str with subStr bolded
+ * @example
+ * boldSubstring('Apple', 'pl'); // returns "Ap<b>pl</b>e"
+ */
 function boldSubstring(str, subStr) {
-
 	// get starting and ending idx of subStr in str
 	const startIdx = str.toLowerCase().indexOf(subStr);
 	const endIdx = startIdx+subStr.length;
 
-	const boldedResult = str.split('').map((val,idx) => {
-		if (idx >= startIdx && idx < endIdx) {
-			return `<b>${val}</b>`;
-		} else {
-			return val;
-		}
-	}).join('');
-	return boldedResult;
+	let strArray = str.split('');
+	strArray.splice(endIdx, 0, '</b>')
+	strArray.splice(startIdx, 0, '<b>');
+
+	return strArray.join('')
 }
 
-// highlights the list item the user is hovering over
+// highlights the list item the user is hovering over, also removes highlight when user removes hover
 function highlightItem(e) {
-	// TODO
+	if (e.type === 'mouseover') {
+		e.target.classList.add('highlighted');
+	}
+	else if (e.type == 'mouseout') {
+		e.target.classList.remove('highlighted');
+	}
 }
 
+// populates input with clicked element's inner text
 function useSuggestion(e) {
-	// TODO
+	suggestions.innerHTML = '';
+	if (e.target.tagName === 'LI') { 	// if user clicks on non-bolded text
+		input.value = e.target.innerText; 	
+	} else {	// if user clicks on bolded text
+		input.value = e.target.parentElement.innerText;	
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('mouseover', )
+suggestions.addEventListener('mouseover', highlightItem);
+suggestions.addEventListener('mouseout', highlightItem)
 suggestions.addEventListener('click', useSuggestion);
