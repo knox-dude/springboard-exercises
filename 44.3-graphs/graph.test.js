5d785f75c246fe589a85055c28e818c3d3c97bb8
Andrew Knox <andrew.knox.r@gmail.com>
2023-10-17 15:11:45 -0700

    Added test for findShortestPath

--- 44.3-graphs/graph.test.js
+++ 44.3-graphs/graph.test.js
@@ -198,3 +198,52 @@ describe("BFS", function() {
     ]);
   });
 });
+
+describe("findShortestPath", function() {
+  it("should return the shortest path from source to target", function() {
+    let graph = new Graph();
+    let S = new Node("S");
+    let P = new Node("P");
+    let U = new Node("U");
+    let X = new Node("X");
+    let Q = new Node("Q");
+    let Y = new Node("Y");
+    let V = new Node("V");
+    let R = new Node("R");
+    let W = new Node("W");
+    let T = new Node("T");
+
+    graph.addVertices([S, P, U, X, Q, Y, V, R, W, T]);
+
+    graph.addEdge(S, P);
+    graph.addEdge(S, U);
+
+    graph.addEdge(P, X);
+    graph.addEdge(U, X);
+
+    graph.addEdge(P, Q);
+    graph.addEdge(U, V);
+
+    graph.addEdge(X, Q);
+    graph.addEdge(X, Y);
+    graph.addEdge(X, V);
+
+    graph.addEdge(Q, R);
+    graph.addEdge(Y, R);
+
+    graph.addEdge(Y, W);
+    graph.addEdge(V, W);
+
+    graph.addEdge(R, T);
+    graph.addEdge(W, T);
+
+    var result = JSON.stringify(graph.findShortestPath(S, T));
+    var validResult =
+    result ===
+      JSON.stringify(["S", "U", "V", "W", "T"]) ||
+    result ===
+      JSON.stringify(["S", "P", "Q", "R", "T"]);
+
+    expect(validResult).toBe(true);
+  });
+});
