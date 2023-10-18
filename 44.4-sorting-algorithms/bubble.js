3a6c4da472ebc38c1f87308457cdb595b1d32e54
Andrew Knox <andrew.knox.r@gmail.com>
2023-10-18 14:44:34 -0700

    added bubblesort

--- 44.4-sorting-algorithms/bubble.js
+++ 44.4-sorting-algorithms/bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(nums) {
+
+  if (nums.length === 0) return [];
+
+  for (let i = nums.length-1; i>-1; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j+1]) {
+        [nums[j], nums[j+1]] = [nums[j+1], nums[j]];
+      }
+    }
+  }
+  return nums;
+}
 
 module.exports = bubbleSort;
