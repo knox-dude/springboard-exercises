2ab0f5ac78c0421766bde2a6273bf5d6d17d253e
Andrew Knox <andrew.knox.r@gmail.com>
2023-10-18 15:08:29 -0700

    added insertion sort

--- 44.4-sorting-algorithms/insertion.js
+++ 44.4-sorting-algorithms/insertion.js
@@ -1,3 +1,14 @@
-function insertionSort() {}
+function insertionSort(arr=[]) {
+  if (arr.length === 0) return arr;
+  
+  for (let i = arr.length; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] < arr[j-1]) {
+        [arr[j], arr[j-1]] = [arr[j-1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
 
 module.exports = insertionSort;
