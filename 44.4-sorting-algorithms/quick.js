0bc7a3718d2a77b1d0450b84c64c8b471d8b43f0
Andrew Knox <andrew.knox.r@gmail.com>
2023-10-18 15:47:19 -0700

    added quicksort with start value pivot

--- 44.4-sorting-algorithms/quick.js
+++ 44.4-sorting-algorithms/quick.js
@@ -3,12 +3,35 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start=0, end=arr.length-1){
+  if (arr.length === 0) return 0;
+
+  let pivot = arr[start], k=end;
+
+  for (let i = end; i > start; i--) {
+    if (arr[i] > pivot){
+      [arr[i], arr[k]] = [arr[k], arr[i]];
+      k--;
+    }
+  }
+
+  [arr[start], arr[k]] = [arr[k], arr[start]]
+
+  return k;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, left=0, right=arr.length-1) {
 
-module.exports = quickSort;
+  if (left < right) {
+    let pi = pivot(arr, left, right);
+    quickSort(arr, left, pi - 1);  // Before pi
+    quickSort(arr, pi + 1, right); // After pi
+  }
+  return arr;
+}
+
+module.exports = {pivot, quickSort};
