b75a9865dc4a6ea7f2ecedc53c62568e6fa40a22
Andrew Knox <andrew.knox.r@gmail.com>
2023-10-24 15:23:12 -0700

    Added finished 19.4 without advanced study

--- 19.4-python-oop/serial.py
+++ 19.4-python-oop/serial.py
@@ -20,3 +20,16 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=100):
+        """Creates a SerialGenerator object"""
+        self.start = start-1
+        self.current = self.start
+    
+    def generate(self):
+        """Generates the next number in the sequence"""
+        self.current += 1
+        return self.current
+    
+    def reset(self):
+        """Resets current value to initial start value"""
+        self.current = self.start
--- 19.4-python-oop/wordfinder.py
+++ 19.4-python-oop/wordfinder.py
@@ -1,5 +1,40 @@
-"""Word Finder: finds random words from a dictionary."""
-
+from random import choice
 
 class WordFinder:
-    ...
+    """Finds random words from a dictionary."""
+
+    def __init__(self, fileDir="words.txt"):
+        """Reads and stores the given dictionary"""
+        self.words = []
+        self.fileDir = fileDir
+        self._hasReadFile = False
+        self._readFile()
+        print(f"{len(self.words)} words read")
+
+    def _readFile(self):
+        """Populates self.words or returns if file has been read"""
+
+        if self._hasReadFile: return
+
+        with open(self.fileDir, "r") as fp:
+            lines = fp.readlines()
+            for line in lines:
+                self.words.append(line.strip())
+
+        self._hasReadFile = True
+
+    def random(self):
+        """Returns a random word from the file that was read"""
+        return choice(self.words) 
+
+
+class SpecialWordFinder(WordFinder):
+    """WordFinder that excludes commented and blank lines"""
+
+    def __init__(self, fileDir="words.txt"):
+        """Reads and stores the given dictionary, skipping blank/commented lines"""
+        super().__init__(fileDir)
+        wordsBefore = len(self.words)
+        self.words = [s for s in self.words if s and not s.startswith("#")]
+        print(f"Removed {wordsBefore-len(self.words)} blank or commented words")
+        
