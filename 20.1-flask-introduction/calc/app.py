c013225b27eddfa1eaeba79d490d35828582e8de
Andrew Knox <andrew.knox.r@gmail.com>
2023-10-24 17:20:47 -0700

    added completed 20.1

--- 20.1-flask-introduction/calc/app.py
+++ 20.1-flask-introduction/calc/app.py
@@ -1,1 +1,24 @@
 # Put your app in here.
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+ops = {"add":operations.add,
+       "sub":operations.sub,
+       "mult":operations.mult,
+       "div":operations.div}
+
+@app.route('/<operation>')
+def operate(operation):
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = ops[operation](a, b)
+  return str(result)
+
+@app.route('/math/<operation>')
+def operate2(operation):
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = ops[operation](a, b)
+  return str(result)
