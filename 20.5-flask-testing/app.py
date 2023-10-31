1e8597bc19bf2888c3ee3b4a1f9c389036ce4ff5
Andrew Knox <andrew.knox.r@gmail.com>
2023-10-31 12:38:24 -0700

    Added a bunch of work - almost have all methods

--- 20.5-flask-testing/app.py
+++ 20.5-flask-testing/app.py
@@ -1,4 +1,5 @@
 from boggle import Boggle
+import pdb
 from flask import Flask, request, render_template, jsonify, redirect, flash, session
 
 app = Flask(__name__)
@@ -10,8 +11,10 @@ boggle_game = Boggle()
 def landing_page():
   board = boggle_game.make_board()
   session["board"] = board
+  score = session.get("highscore", 0)
+  nplays = session.get("nplays", 0)
 
-  return render_template("board.html", board=board)
+  return render_template("board.html", board=board, highscore=score, nplays=nplays)
 
 @app.route("/check-guess")
 def check_guess():
@@ -22,3 +25,16 @@ def check_guess():
 
   result = boggle_game.check_valid_word(board, guess)
   return jsonify({"result": result})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+  """Saves users high score and number of games"""
+  score = request.json["score"]
+  highscore = session.get("highscore", 0)
+  nplays = session.get("nplays", 0)
+
+  session['nplays'] = nplays + 1
+  newRecord = True if score > highscore else False
+
+  session['highscore'] = max(score, highscore)
+  return jsonify(newRecord)
--- 20.5-flask-testing/static/boggle.js
+++ 20.5-flask-testing/static/boggle.js
@@ -1,15 +1,111 @@
 class Boggle {
 
-  constructor() {
+  /**
+   * Builds the boggle object
+   * 
+   * @param {number} [seconds=60] The amount of time the user has to guess words
+   */
+  constructor(seconds=60) {
     this.board = $("#boggle");
-    $(".guess-form", this.board).on("submit", this.guess.bind(this));
+    this.seconds=seconds
+    this.score = 0;
+    this.correct_guesses = new Set();
+
+    this.timer = setInterval(this.tick.bind(this), 1000);
+
+    $(".guess-form", this.board).on("submit", this.handle_submit_guess.bind(this));
   }
   
-  async guess(evt) {
+  /**
+   * Handles a submit guess event from the .guess form
+   * 
+   * @param {Event} evt 
+   */
+  async handle_submit_guess(evt) {
     evt.preventDefault();
+
     const $guess = $(".guess", this.board);
-    let guess = $guess.val();
+    let guess = $guess.val().toLowerCase();
+
     const response = await axios.get("/check-guess", {params: {guess:guess}})
-    $(".msg", this.board).text(`You guessed ${guess} and the response was ${response.data.result}`)
+    const text_of_response = response.data.result
+
+    if (this.correct_guesses.has(guess)) {
+      this.show_message(guess, text_of_response, true)
+    } else {
+      this.show_message(guess, text_of_response)
+    }
+  
+    if (text_of_response === "ok" && !(this.correct_guesses.has(guess))) {
+      this.correct_guess(guess);
+    } 
+  }
+
+  /**
+   * Handles a correct guess from the user
+   * 
+   * @param {String} guess - the user's guess in text
+   */
+  correct_guess(guess) {
+    $(".correct-guesses", this.board).append(`<li>${guess.toUpperCase()} - ${guess.length} points</li>`)
+    this.score += guess.length;
+    this.correct_guesses.add(guess)
+    $("#score", this.board).text(this.score);
+  }
+
+  /**
+   * Shows the message after a user's guess
+   * 
+   * @param {String} guess - the user's guess
+   * @param {String} response - the response to user's guess (ok, not-word, or not-on-board)
+   * @param {Boolean} already_guessed - true if user has input guess before
+   */
+  show_message(guess, response, already_guessed=false) {
+    guess = guess.toUpperCase();
+    if (already_guessed) {
+      $(".msg", this.board).text(`You've already guessed ${guess}`)
+      return
+    }
+    if (response === "not-word") {
+      $(".msg", this.board).text(`You guessed ${guess} which is not a word.`)
+    } else if (response === "not-on-board") {
+      $(".msg", this.board).text(`You guessed ${guess} which is not a possible word for this board.`)
+    } else {
+      $(".msg", this.board).text(`You guessed ${guess} - It's a correct word!!`)
+    }
+  }
+
+  /**
+   * Updates the timer on the frontend
+   */
+  show_timer() {
+    $("#timer", this.board).text(this.seconds);
+  }
+
+  /**
+   * Ticks timer down and handles when timer is 0
+   */
+  async tick() {
+    this.seconds -= 1;
+    this.show_timer();
+
+    if (this.seconds === 0) {
+      clearInterval(this.timer);
+      await this.post_score();
+    }
+  }
+
+  /**
+   * Sends a post request with user's high score
+   */
+  async post_score() {
+    $(".guess-form", this.board).hide();
+    const response = await axios.post("/post-score", {score:this.score});
+    console.log(response.data);
+    if (response.data) {
+      $(".msg", this.board).text(`New high score: ${this.score}`)
+    } else {
+      $(".msg", this.board).text(`Final score: ${this.score}`)
+    }
   }
 }
