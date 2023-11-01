67743f0aa5ee7363ceb70d4565425779835ea2dc
Andrew Knox <andrew.knox.r@gmail.com>
2023-11-01 15:51:07 -0700

    added find-rotated-index

--- 20.6-divide-and-conquer/find-rotated-index.js
+++ 20.6-divide-and-conquer/find-rotated-index.js
@@ -48,12 +48,26 @@ findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
  */
 
 function findRotatedIndex(arr, val) {
-  let left=0, right=0, mid=-1
-  let pivot = findPivot(arr)
-  console.log()
-  // if (arr[pivot] < )
-  return 1
+  let left=0, right=arr.length-1, mid=-1
 
+  let pivot = findPivot(arr)
+  if (arr[pivot] == val) return pivot;
+
+  if (arr[pivot] < val && val < arr[right]) return binarySearch(arr, val, pivot, right);
+  if (arr[pivot] < val && val > arr[right]) return binarySearch(arr, val, left, pivot-1);
+
+}
+
+function binarySearch(arr, val, left=0, right=arr.length-1) {
+  if (right-left <= 1) {
+    if (arr[left] != val && arr[right] != val) return -1;
+    return arr[left]===val ? left : right
+  } 
+
+  let mid = Math.floor((right-left)/2) + left
+  if (arr[mid] == val) return mid;
+  if (arr[mid] < val) return binarySearch(arr, val, mid, right)
+  if (arr[mid] > val) return binarySearch(arr, val, left, mid)
 }
 
 function findPivot(arr, left=0, right=arr.length-1) {
@@ -61,21 +75,19 @@ function findPivot(arr, left=0, right=arr.length-1) {
   if (right-left <= 1) {
     console.log("implement base case");
   }
-  let mid = Math.floor((right-left)/2)
+  let mid = Math.floor((right-left)/2) + left
   if (arr[mid+1] < arr[mid]) {
     return mid+1
   }
   if (arr[mid] > arr[right] && arr[mid] > arr[left]) {
-    return findPivot(arr, left+mid, right);
+    return findPivot(arr, mid, right);
   }
   else if (arr[mid] < arr[right] && arr[mid] < arr[left]) {
-    return findPivot(arr, left, right-mid);
+    return findPivot(arr, left, mid);
   }
   else {
-    console.log("uhhhhhh...");
+    return -1 //couldn't find pivot
   }
-
-
 }
 
 module.exports = findRotatedIndex
