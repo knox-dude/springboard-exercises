1199cc616bb295c6928d2be186bb28c7e15b4570
Andrew Knox <andrew.knox.r@gmail.com>
2023-11-01 16:08:20 -0700

    deleted sorted-frequency, reworking solution

--- 20.6-divide-and-conquer/sorted-frequency.js
+++ 20.6-divide-and-conquer/sorted-frequency.js
@@ -12,31 +12,6 @@ Time Complexity: O(log N)
  */
 
 function sortedFrequency(arr, value) {
-  let left = 0;
-  let right = arr.length-1;
-  let mid = -1;
-
-  while (right-left > 1) {
-    mid = Math.floor((right-left)/2) + left;
-
-    if (arr[mid] < value) left = mid;
-
-    else if (arr[mid] > value) right = mid;
-
-    else if (arr[mid] == value) {
-      let leftMid = mid, rightMid = mid;
-      while (arr[leftMid] == value) {
-        leftMid--
-      }
-      while (arr[rightMid] == value) {
-        rightMid++
-      }
-      return rightMid - leftMid - 1;
-    }
-  }
-  if (arr[left] > value || arr[right] < value) return -1;
-  if (arr[left] == value && arr[right] > value) return 1;
-  if (arr[right] == value && arr[left] < value) return 1;
 }
 
 module.exports = sortedFrequency
