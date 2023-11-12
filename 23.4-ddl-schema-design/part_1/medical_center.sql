fbc54e1dba203a686e371c07e1b8587a54dc849c
Andrew Knox <andrew.knox.r@gmail.com>
2023-11-12 15:34:46 -0800

    changed med_center around

--- 23.4-ddl-schema-design/part_1/medical_center.sql
+++ 23.4-ddl-schema-design/part_1/medical_center.sql
@@ -1,38 +1,70 @@
-DROP DATABASE IF EXISTS medical_center;
+DROP DATABASE IF EXISTS med_center_db;
+CREATE DATABASE med_center_db;
 
-CREATE DATABASE medical_center;
+\c med_center_db
 
-\c medical_center
+CREATE TABLE diagnosis
+(
+  id      SERIAL PRIMARY KEY,
+  visit_id INTEGER NOT NULL
+);
+
+CREATE TABLE disease
+(
+  id           SERIAL PRIMARY KEY,
+  diagnosis_id INTEGER NOT NULL,
+  disease_name TEXT
+);
 
 CREATE TABLE doctor
 (
-  id SERIAL PRIMARY KEY,
-  
-  
-  
-  id SERIAL PRIMARY KEY,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  seat TEXT NOT NULL,
-  departure TIMESTAMP NOT NULL,
-  arrival TIMESTAMP NOT NULL,
-  airline TEXT NOT NULL,
-  from_city TEXT NOT NULL,
-  from_country TEXT NOT NULL,
-  to_city TEXT NOT NULL,
-  to_country TEXT NOT NULL
+  id          SERIAL PRIMARY KEY,
+  center_id   INTEGER NOT NULL,
+  doctor_name TEXT
 );
 
-INSERT INTO tickets
-  (first_name, last_name, seat, departure, arrival, airline, from_city, from_country, to_city, to_country)
-VALUES
-  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
-  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
-  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
-  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
-  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
-  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
-  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
-  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
-  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
-  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+CREATE TABLE med_center
+(
+  id          SERIAL PRIMARY KEY,
+  center_name TEXT 
+);
+
+CREATE TABLE patient
+(
+  id         SERIAL PRIMARY KEY,
+  first_name TEXT,
+  last_name  TEXT
+);
+
+CREATE TABLE visits
+(
+  id         SERIAL PRIMARY KEY,
+  doctor_id  INTEGER NOT NULL,
+  patient_id INTEGER NOT NULL,
+  visit_date DATE NOT NULL
+);
+
+ALTER TABLE doctor
+  ADD CONSTRAINT FK_center
+    FOREIGN KEY (center_id)
+    REFERENCES med_center (id);
+
+ALTER TABLE visits
+  ADD CONSTRAINT FK_doctor
+    FOREIGN KEY (doctor_id)
+    REFERENCES doctor (id);
+
+ALTER TABLE visits
+  ADD CONSTRAINT FK_patient
+    FOREIGN KEY (patient_id)
+    REFERENCES patient (id);
+
+ALTER TABLE diagnosis
+  ADD CONSTRAINT FK_visits
+    FOREIGN KEY (visit_id)
+    REFERENCES visits (id);
+
+ALTER TABLE disease
+  ADD CONSTRAINT FK_diagnosis
+    FOREIGN KEY (diagnosis_id)
+    REFERENCES diagnosis (id);
