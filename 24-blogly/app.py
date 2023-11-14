3a73cf09f3d609362003d7d4ac9b8474333aad9f
Andrew Knox <andrew.knox.r@gmail.com>
2023-11-13 18:27:29 -0800

    added templates and some routes

--- 24-blogly/app.py
+++ 24-blogly/app.py
@@ -9,4 +9,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+
+@app.route("/")
+def home_page():
+  """Redirects to list of users"""
+  return redirect("/users")
+
+@app.route("/users/new", methods=["POST"])
+def add_user():
+  """Add user and redirect to list."""
+
+  first_name = request.form['first_name']
+  last_name = request.form['last_name']
+  image_url = request.form['image_url']
+
+  user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+  db.session.add(user)
+  db.session.commit()
+
+  return redirect("/users")
+
+@app.route("/users")
+def list_users():
+  """List users and show link to add form."""
+  users = User.query.all()
+  return render_template("list.html", users=users)
