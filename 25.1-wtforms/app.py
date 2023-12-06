1846ec4a5b88afb067378bba15858658e09c3cb9
Andrew Knox <andrew.knox.r@gmail.com>
2023-12-06 13:13:18 -0800

    added hidden secret key

--- 25.1-wtforms/app.py
+++ 25.1-wtforms/app.py
@@ -3,10 +3,11 @@
 from flask import Flask, request, redirect, render_template, flash
 from models import db, connect_db, Pet
 from forms import AddPetForm, EditPetForm
+from secret import SECRET_KEY
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "as24ejx9D12n!adsSDN028D"
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_adoption'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
