d5e75f5c05d83afa557853ef6cc046fee3514cdc
Andrew Knox <andrew.knox.r@gmail.com>
2023-12-11 12:08:21 -0800

    added completed project

--- 25.5-linked-lists/linked-list.js
+++ 25.5-linked-lists/linked-list.js
@@ -23,7 +23,7 @@ class LinkedList {
   push(val) {
     const node = new Node(val);
     //case 0: no elms
-    if (this.tail === null && this.head === null) { 
+    if (!this.head) { 
       this.tail = node;
       this.head = node;
     } 
@@ -45,7 +45,7 @@ class LinkedList {
   unshift(val) {
     const node = new Node(val);
     //case 0: no elms
-    if (this.head === null) {
+    if (!this.head) {
       this.tail = node;
       this.head = node;
     }
@@ -67,8 +67,8 @@ class LinkedList {
 
   pop() {
     //case 0: no elms
-    if (this.head === null) {
-      return null
+    if (!this.head) {
+      throw new Error('Cannot pop from an empty list');
     }
     //case 1: 1 elm
     else if (this.head === this.tail) {
@@ -76,7 +76,7 @@ class LinkedList {
       this.tail = null;
       this.head = null;
       this.length -= 1;
-      return temp;
+      return temp.val;
     }
     //case 2: 2+ elms
     else {
@@ -89,46 +89,153 @@ class LinkedList {
       this.tail = prev;
       prev.next = null;
       this.length--;
-      return curr;
+      return curr.val;
     }
-
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
-    const temp = this.head;
-    this.head = this.head.next;
+    //case 0: no elms
+    if (!this.head) {
+      throw new Error('Cannot shift from an empty list')
+    }
+    //case 1: 1 elm
+    else if (this.head === this.tail) {
+      const temp = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return temp.val;
+    }
+    //case 2: 2+ elms
+    else {
+      const temp = this.head;
+      this.head = this.head.next;
+      this.length--;
+      return temp.val;
+    }
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-
+    // if invalid index, throw error
+    if (idx >= this.length || idx < 0) {
+      throw new Error(`Invalid index: ${idx}`)
+    }
+    //iterate to idx and return
+    let curr = this.head;
+    let counter = 0;
+    while (counter != idx) {
+      if (curr === null) {
+        throw new Error("Unexpected error in getAt")
+      }
+      curr = curr.next;
+      counter++;
+    }
+    return curr.val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-
+    // if invalid index, throw error
+    if (idx >= this.length || idx < 0) {
+      throw new Error(`Invalid index: ${idx}`)
+    }
+    let curr = this.head;
+    let counter = 0;
+    while (counter != idx) {
+      if (curr === null) {
+        throw new Error("Unexpected error in setAt")
+      }
+      curr = curr.next;
+      counter++;
+    }
+    curr.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
-
+    // if invalid index, throw error
+    if (idx > this.length || idx < 0) {
+      throw new Error(`Invalid index: ${idx}`)
+    }
+    // inserting at beginning
+    if (idx === 0) {
+      this.unshift(val);
+    }
+    // inserting at end
+    else if (idx === this.length) {
+      this.push(val);
+    }
+    // inserting somewhere in the middle (non-optimized)
+    else {
+      let prev = this.head, curr = prev.next;
+      let counter = 1;
+      while (counter != idx) {
+        if (curr === null) {
+          throw new Error("Unexpected error in insertAt")
+        }
+        prev = curr, curr = curr.next;
+        counter++;
+      }
+      let newNode = new Node(val);
+      prev.next = newNode, prev.next.next = curr;
+      this.length++;
+    }
+    return;
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
-
+    // if invalid index, throw error
+    if (idx > this.length || idx < 0) {
+      throw new Error(`Invalid index: ${idx}`)
+    }
+    // removing at beginning
+    if (idx === 0) {
+      return this.shift();
+    }
+    // removing at end
+    else if (idx === this.length) {
+      return this.pop();
+    }
+    // removing somewhere in the middle (non-optimized)
+    else {
+      let prev = this.head, curr = prev.next;
+      let counter = 1;
+      while (counter != idx) {
+        if (curr === null) {
+          throw new Error("Unexpected error in insertAt")
+        }
+        prev = curr, curr = curr.next;
+        counter++;
+      }
+      let newNext = curr.next;
+      prev.next = newNext;
+      this.length--;
+      return curr.val;
+    }
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
+    //returns 0 for empty lists
+    if (this.length === 0) {
+      return 0
+    }
     
+    let run_sum = 0, curr = this.head;
+    while (curr) {
+      run_sum += curr.val;
+      curr = curr.next;
+    }
+    return run_sum / this.length;
   }
 }
 
