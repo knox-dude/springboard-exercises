3116d198c227f52f51b50caa95022eab1a8bc608
Andrew Knox <andrew.knox.r@gmail.com>
2024-03-13 14:21:37 -0700

    minor change to add visibility

--- 36.1-hashing-and-JWTs/models/user.js
+++ 36.1-hashing-and-JWTs/models/user.js
@@ -14,8 +14,8 @@ class User {
    */
 
   static async register({username, password, first_name, last_name, phone}) {
-    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     try {
+      const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
       const result = await db.query(
           `INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
            VALUES ($1, $2, $3, $4, $5, NOW(), NOW())
