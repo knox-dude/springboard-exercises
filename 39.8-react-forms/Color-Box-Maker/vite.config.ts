440e7cb850a4ed6a3a48fe0021f60a95fb023115
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-06 11:30:08 -0700

    trying to get stupid fucking tests to work in vite. fuck you springboard

--- 39.8-react-forms/Color-Box-Maker/vite.config.ts
+++ 39.8-react-forms/Color-Box-Maker/vite.config.ts
@@ -6,7 +6,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+  },
+  plugins: [
+    react(),
+    ],
   test:
   {
     globals: true,
