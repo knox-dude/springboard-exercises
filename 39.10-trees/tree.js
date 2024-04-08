bc9f9c1b8a372bc99d80e4faf055ccd0dc25d1bc
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-08 14:48:48 -0700

    added completed tree class

--- 39.10-trees/tree.js
+++ 39.10-trees/tree.js
@@ -15,13 +15,39 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+
+    // function that recursively adds all of the values
+    function recursiveSum(node) {
+      if (!node) return 0;
+      let sum = 0;
+      for (let child of node.children) {
+        sum += recursiveSum(child);
+      }
+      return node.val + sum;
+    }
+
+    return recursiveSum(this.root);
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
 
+    // function that recursively counts the number of nodes with val % 2 === 0
+    function recursiveEvenCount(node) {
+      if (!node) return 0;
+      let count = 0;
+      for (let child of node.children) {
+        count += recursiveEvenCount(child);
+      }
+      if (node.val % 2 === 0) {
+        return count + 1;
+      } else {
+        return count;
+      }
+    }
+
+    return recursiveEvenCount(this.root);
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
@@ -29,6 +55,21 @@ class Tree {
 
   numGreater(lowerBound) {
 
+    // function that recursively counts the number of nodes with val > lowerBound
+    function recursiveNumGreater(node) {
+      if (!node) return 0;
+      let count = 0;
+      for (let child of node.children) {
+        count += recursiveNumGreater(child);
+      }
+      if (node.val > lowerBound) {
+        return count + 1;
+      } else {
+        return count;
+      }
+    }
+
+    return recursiveNumGreater(this.root);
   }
 }
 
