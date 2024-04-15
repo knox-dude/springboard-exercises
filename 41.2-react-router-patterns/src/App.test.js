221d1ca06abb0d8037ebc5d51a0a90fbff782932
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-15 12:47:22 -0700

    adding starter code

---
+++ 41.2-react-router-patterns/src/App.test.js
@@ -0,0 +1,10 @@
+import Router from "react-router";
+import DogDetails from './DogDetails';
+import { render, screen, waitFor } from '@testing-library/react';
+
+Router.useParams = jest.fn();
+
+test('renders learn react link', () => {
+  Router.useParams.mockReturnValue({a: "b"});	
+  render(<DogDetail />)
+});
