95957f2309f446ebd769efe2958f2cc0c8be98c4
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-21 15:39:38 -0700

    finished is-subseq

--- 45.1-problem-solving-patterns/is-subsequence.js
+++ 45.1-problem-solving-patterns/is-subsequence.js
@@ -1,3 +1,12 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(s1, s2) {
+  let s1Ptr = 0;
+  for (let c of s2) {
+    if (s1Ptr >= s1.length) return true
+    if (s1[s1Ptr] == c) s1Ptr++;
+  }
+  if (s1Ptr >= s1.length) return true;
+  return false;
 }
+
+module.exports = isSubsequence;
