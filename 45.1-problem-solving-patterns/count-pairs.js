62c770f6ae6ee430eb6830049fc711a52b115f8c
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-21 15:42:26 -0700

    finished count-pairs

--- 45.1-problem-solving-patterns/count-pairs.js
+++ 45.1-problem-solving-patterns/count-pairs.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(nums, target) {
+  let s = new Set()
+  let ans = 0;
+  for (let num of nums) {
+    if (s.has(num)) {
+      ans++;
+    } else {
+      s.add(target-num)
+    }
+  }
+  return ans
 }
+
+module.exports = countPairs;
