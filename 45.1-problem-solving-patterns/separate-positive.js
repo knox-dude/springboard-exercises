6ae94d518e4e2eeab9ad9e44e5d76c79fe40ab20
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-21 15:35:16 -0700

    finished separate-positive

--- 45.1-problem-solving-patterns/separate-positive.js
+++ 45.1-problem-solving-patterns/separate-positive.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+  let l = 0, r = nums.length-1;
+  while (l < r) {
+    if (nums[l] < 0 && nums[r] > 0) {
+      [nums[l], nums[r]] = [nums[r], nums[l]];
+      l++; r--;
+    } else if (nums[l] < 0) {
+      r--;
+    } else {
+      l++;
+    }
+  }
+  return nums;
 }
+
+module.exports = separatePositive
