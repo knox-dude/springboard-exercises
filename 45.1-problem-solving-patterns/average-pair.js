0b1b670063f73c420338e69cff442fb94d1d95ba
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-21 15:11:25 -0700

    finished code for average-pair and construct-note, added import and export statements for jest

--- 45.1-problem-solving-patterns/average-pair.js
+++ 45.1-problem-solving-patterns/average-pair.js
@@ -1,3 +1,11 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(nums, target) {
+  let s = new Set()
+  for (let num of nums) {
+    if (s.has(num)) return true
+    s.add((target*2)-num)
+  }
+  return false
 }
+
+module.exports = averagePair
--- 45.1-problem-solving-patterns/construct-note.js
+++ 45.1-problem-solving-patterns/construct-note.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function constructNote(str1, str2, ) {
+function constructNote(msg, letters) {
+  // make dict from letters
+  dict = {}
+  for (let letter of letters) {
+    dict[letter] ? dict[letter]++ : dict[letter] = 1
+  }
+  for (let c of msg) {
+    if (dict[c]) {
+      dict[c]--;
+    } else {
+      return false
+    }
+  }
+  return true
 }
+
+module.exports = constructNote
