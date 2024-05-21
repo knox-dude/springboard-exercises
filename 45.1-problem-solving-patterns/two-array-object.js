c0e25f1badeb0ce549cf335271eeac44ecd04350
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-21 15:14:26 -0700

    finished two-array-object

--- 45.1-problem-solving-patterns/two-array-object.js
+++ 45.1-problem-solving-patterns/two-array-object.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, vals) {
+  ans = {}
+  for (let i = 0; i < keys.length; i++) {
+    if (i >= vals.length) {
+      ans[keys[i]] = null;
+    } else {
+      ans[keys[i]] = vals[i];
+    }
+  }
+
+  return ans
 }
+
+module.exports = twoArrayObject
