ed836bb58aba8e3182111cf9d6a6e0179609e191
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-23 16:37:55 -0700

    added create-account solution

--- 45.3-js-tricky-parts/create-account.js
+++ 45.3-js-tricky-parts/create-account.js
@@ -1,5 +1,26 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount=0) {
+  return {
+    checkBalance(inputPin) {
+      if (inputPin !== pin) return "Invalid PIN."
+      return `$${amount}`
+    },
+    deposit(inputPin, depositAmt) {
+      if (inputPin !== pin) return "Invalid PIN."
+      amount += depositAmt
+      return `Succesfully deposited $${depositAmt}. Current balance: $${amount}.`
+    },
+    withdraw(inputPin, withdrawAmt) {
+      if (inputPin !== pin) return "Invalid PIN."
+      if (withdrawAmt > amount) return "Withdrawal amount exceeds account balance. Transaction cancelled."
+      amount -= withdrawAmt
+      return `Succesfully withdrew $${withdrawAmt}. Current balance: $${amount}.`
+    },
+    changePin(inputPin, newPin) {
+      if (inputPin !== pin) return "Invalid PIN."
+      pin = newPin
+      return "PIN successfully changed!"
+    }
+  }
 }
 
 module.exports = { createAccount };
