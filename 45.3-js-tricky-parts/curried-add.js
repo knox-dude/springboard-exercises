ac274555cfbab64847d95fcf012c5d0e64b8bee6
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-23 16:50:43 -0700

    added curried-add solution

--- 45.3-js-tricky-parts/curried-add.js
+++ 45.3-js-tricky-parts/curried-add.js
@@ -1,5 +1,10 @@
 function curriedAdd(total) {
-
+  if (!total) return 0;
+  return function add(num) {
+    if (!num) return total;
+    total += num;
+    return add;
+  }
 }
 
 module.exports = { curriedAdd };
