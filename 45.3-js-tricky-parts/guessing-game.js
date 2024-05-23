861b54253f81222e7f8518ee3dfd87d7211dd99d
Andrew Knox <andrew.knox.r@gmail.com>
2024-05-23 16:26:33 -0700

    added guessing-game solution

--- 45.3-js-tricky-parts/guessing-game.js
+++ 45.3-js-tricky-parts/guessing-game.js
@@ -1,4 +1,21 @@
 function guessingGame() {
+  const numToGuess = Math.floor(Math.random() * 100);
+  let numGuesses = 0;
+  let gameOver = false;
+  return function(guess) {
+    if (gameOver) {
+      return "The game is over, you already won!";
+    }
+    numGuesses++;
+    if (guess === numToGuess) {
+      gameOver = true;
+      return `You win! You found ${guess} in ${numGuesses} guesses.`
+    } else if (guess < numToGuess) {
+      return (`${guess} is too low!`)
+    } else {
+      return (`${guess} is too high!`)
+    }
+  }
 
 }
 
